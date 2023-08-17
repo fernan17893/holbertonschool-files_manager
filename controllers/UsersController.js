32ce1cbd76fee0504e3ef185930c5f7aeb9e6307
fernan17893 <fernan17893@gmail.com>
2023-08-16 20:08:32 -0400

    another try push

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,14 +7,23 @@ export default class UsersController {
   static async postNew(req, res) {
     const { email, password } = req.body;
 
-    if (!email) return res.status(400).send({ error: 'Missing email' });
-    if (!password) return res.status(400).send({ error: 'Missing password' });
-    const emailExists = await dbClient.users.findOne({ email });
-    if (emailExists) return res.status(400).send({ error: 'Already exist' });
+    if (!email) {
+      return res.status(400).send({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).send({ error: 'Missing password' });
+    }
+
+    const emailExists = await dbClient.usersCollection.findOne({ email });
+
+    if (emailExists) {
+      return res.status(400).send({ error: 'Already exist' });
+    }
 
     const secPass = sha1(password);
 
-    const insertStat = await dbClient.users.insertOne({
+    const insertStat = await dbClient.usersCollection.insertOne({
       email,
       password: secPass,
     });
